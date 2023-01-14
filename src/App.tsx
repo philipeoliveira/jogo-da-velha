42f66af33222b9139f56bf3ba534d45b0667e18e
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2023-01-14 20:51:25 -0300

    Add getCellPlayer() and getWinner() function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 type Players = 'O' | 'X';
@@ -19,14 +18,65 @@ function App() {
       setTurn((prev) => (prev === 'O' ? 'X' : 'O'));
    };
 
+   const getCellPlayer = (index: number) => {
+      if (!choices[index]) {
+         return;
+      }
+
+      return choices[index];
+   };
+
+   const getWinner = () => {
+      const linesToWin = [
+         [0, 1, 2],
+         [3, 4, 5],
+         [6, 7, 8],
+
+         [0, 3, 6],
+         [1, 4, 7],
+         [2, 5, 8],
+
+         [0, 4, 8],
+         [2, 4, 6],
+      ];
+
+      for (const line of linesToWin) {
+         const [a, b, c] = line;
+
+         if (
+            choices[a] &&
+            choices[a] === choices[b] &&
+            choices[a] === choices[c]
+         ) {
+            return choices[a];
+         }
+      }
+   };
+
+   useEffect(() => {
+      const winner = getWinner();
+
+      if (winner) {
+         setWinner(winner);
+      }
+   }, [choices]);
+
    return (
       <div className='game-container'>
-         <p>É a vez do jogador X</p>
-         <p>O jogador X ganhou!</p>
-         <p>Empate</p>
+         <p>É a vez do jogador {turn}</p>
+         {winner && <p>{winner} ganhou!</p>}
+         {/* <p>Empate</p> */}
          <div className='game-board'>
             {createCells().map((_, i) => (
-               <div className='board-cell' key={i} onClick={() => play(i)}>
+               <div
+                  className={`board-cell${
+                     getCellPlayer(i) === undefined
+                        ? ''
+                        : ' player-' + getCellPlayer(i)
+                  }`}
+                  key={i}
+                  onClick={() => play(i)}
+               >
                   {choices[i]}
                </div>
             ))}
