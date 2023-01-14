c8d675d1cdbbf3e17b4cbd5888bde2dbc69cfd9f
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2023-01-14 19:55:47 -0300

    create play() function

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
 
+type Players = 'O' | 'X';
+
 function App() {
+   const [turn, setTurn] = useState<Players>('O');
+   const [winner, setWinner] = useState<Players | null>(null);
+   const [draw, setDraw] = useState<boolean | null>(null);
+   const [choices, setChoices] = useState<{ [key: string]: Players }>({});
+
    const createCells = () => {
       return new Array(9).fill(true);
+   };
+
+   const play = (index: number) => {
+      setChoices((prev) => ({ ...prev, [index]: turn }));
+      setTurn((prev) => (prev === 'O' ? 'X' : 'O'));
    };
 
    return (
@@ -14,7 +26,9 @@ function App() {
          <p>Empate</p>
          <div className='game-board'>
             {createCells().map((_, i) => (
-               <div className='board-cell' key={i}></div>
+               <div className='board-cell' key={i} onClick={() => play(i)}>
+                  {choices[i]}
+               </div>
             ))}
          </div>
          <button>Reiniciar o jogo</button>
